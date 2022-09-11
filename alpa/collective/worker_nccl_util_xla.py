671b7ec96ba2e2f327a1a63886f901456a1dc4a4
ZYHowell <zhuangyh@sjtu.edu.cn>
2022-09-11 23:16:23 +0000

    add fetch and sync d2d stream

--- alpa/collective/worker_nccl_util_xla.py
+++ alpa/collective/worker_nccl_util_xla.py
@@ -78,8 +78,12 @@ def recv_tile(worker, uuid: int, device_id: int,
         n_elements = np.prod(slice_shape)
         recv_stream = col.get_stream(group_name, device_id, True)
         working_stream = xe.fetch_working_streams_from_pyclient(worker.backend)[device_id]
+        # device_put uses d2d stream on src_device
+        stream = xe.fetch_d2d_stream(worker.backend, 0)
 
         event = mark_event(working_stream, device_id)
+        synchronize_one_event(event, recv_stream)
+        event = mark_event(stream, 0)
         synchronize_one_event(event, recv_stream)
 
         col.recv_multigpu(to_recv,
